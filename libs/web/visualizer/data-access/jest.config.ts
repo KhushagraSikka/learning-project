1fc1c19b095ed9d14eb6a279f6f0316fa26fec6d
KhushagraSikka <khushagrasikka@gmail.com>
2025-05-12 16:35:42 +0000

    Final feature additions

---
+++ libs/web/visualizer/data-access/jest.config.ts
@@ -0,0 +1,17 @@
+/* eslint-disable */
+export default {
+  displayName: 'web-visualizer-data-access',
+  preset: '../../../../jest.preset.js',
+  globals: {
+    'ts-jest': { tsconfig: '<rootDir>/tsconfig.spec.json' }
+  },
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]sx?$': 'ts-jest'
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../../../coverage/libs/web/visualizer/data-access'
+};
+
+
+// Potential improvement: Add error handling
