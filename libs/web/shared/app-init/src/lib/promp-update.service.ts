9f12f6bb6e82ece0974669a618c1f7ab8253dfb4
KhushagraSikka <khushagrasikka@gmail.com>
2025-05-24 16:51:02 +0000

    Final feature additions

---
+++ libs/web/shared/app-init/src/lib/promp-update.service.ts
@@ -0,0 +1,35 @@
+import { DOCUMENT } from '@angular/common';
+import { Inject, Injectable } from '@angular/core';
+import { SwUpdate, VersionEvent } from '@angular/service-worker';
+import { Observable, of } from 'rxjs';
+import { filter, tap } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PromptUpdateService {
+  constructor(private updates: SwUpdate, @Inject(DOCUMENT) private readonly document: Document) {}
+
+  forceUpdate(): Observable<VersionEvent | null> {
+    if (!this.updates.isEnabled) {
+      return of(null);
+    }
+    return this.updates.versionUpdates.pipe(
+      filter((evt) => evt.type === 'VERSION_READY'),
+      tap(() => {
+        this.updates.activateUpdate().then(() => {
+          if (
+            confirm(
+              `There is a new version of Angular Spotify available! Would you like to upgrade now?`
+            )
+          ) {
+            this.document.location.reload();
+          }
+        });
+      })
+    );
+  }
+}
+
+
+// TODO: Consider refactoring for better performance
