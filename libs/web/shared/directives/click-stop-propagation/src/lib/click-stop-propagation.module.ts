c37a61765a8dd0cfbc3c6a1cd9bd015da7465ec4
KhushagraSikka <khushagrasikka@gmail.com>
2025-06-05 17:08:52 +0000

    Update project documentation

---
+++ libs/web/shared/directives/click-stop-propagation/src/lib/click-stop-propagation.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ClickStopPropagationDirective } from './click-stop-propagation.directive';
+
+@NgModule({
+  imports: [CommonModule],
+  declarations: [ClickStopPropagationDirective],
+  exports: [ClickStopPropagationDirective]
+})
+export class ClickStopPropagationModule {}
+
+
+// Note: Could be optimized in future iterations
