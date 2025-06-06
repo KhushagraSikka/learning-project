4ce526c8b5c6a934f8f006034d93bbf319ce3d88
KhushagraSikka <khushagrasikka@gmail.com>
2025-06-06 18:50:06 +0000

    Final improvements and cleanup

---
+++ apps/angular-spotify/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'angular-spotify-root',
+  template: ` <router-outlet></router-outlet>`,
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {}
+
+
+// Note: Could be optimized in future iterations
